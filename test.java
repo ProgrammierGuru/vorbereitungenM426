316f4c5c6c8b66c62554154378d4a3739a923c25
Your Name <you@example.com>
2020-11-09 21:17:17 +0100

    Veränderungen am test.java File

--- test.java
+++ test.java
@@ -1,1 +1,13 @@
-public class test{}
+public class test {
+	private int num1;
+
+	public void setNum1(int num1)
+	{
+		this.num1 = num1;
+	}
+	
+	public int getNum1()
+	{
+		return this.num1;
+	}
+}
